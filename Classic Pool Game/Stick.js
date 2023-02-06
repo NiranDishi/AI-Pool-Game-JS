2ed511dfaa3587ccc7e4fe73dd5abb17ee9f7649
AviyaDavid <aviyada@ac.sce.ac.il>
2023-02-06 23:14:16 +0200

    finished inmplementing the physics and collisions with table and balls

--- Classic Pool Game/Stick.js
+++ Classic Pool Game/Stick.js
@@ -1,13 +1,13 @@
 const STICK_ORIGIN = new Vector2(970,11);
 const STICK_SHOT_ORIGIN = new Vector2(950,11);
-
+const MAX_POWER= 7500;
 function Stick(position,onShoot){
     this.position = position;
     this.rotation = 0 ;
     this.origin = STICK_ORIGIN.copy();
     this.power=0;
     this.onShoot = onShoot;
-    
+    this.shot = false;
 }
 
 Stick.prototype.update = function(){
@@ -34,12 +34,22 @@ Stick.prototype.updateRotation = function(){
 }
 
 Stick.prototype.increasePower = function(){
-    this.power +=100;
+    if(this.power > MAX_POWER){
+        return;
+    }
+    this.power +=120;
     this.origin.x+=5;
 }
 
 Stick.prototype.shoot=function(){
     this.onShoot(this.power,this.rotation);
     this.power=0;
-    this.origin=STICK_SHOT_ORIGIN;
+    this.origin=STICK_SHOT_ORIGIN.copy();
+    this.shot= true;
+}
+
+Stick.prototype.reposition = function(position){
+    this.position = position.copy();
+    this.origin= STICK_ORIGIN.copy();
+    this.shot = false;
 }
